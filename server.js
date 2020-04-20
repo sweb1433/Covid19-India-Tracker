fedba246cf183d1929a6874a92c7fd43921697ed
sweb1433 <sweb1433@gmail.com>
2020-04-20 21:48:44 +0530

    minor changes

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static('./dist/read-local-json-angular'));
+
+app.get('/*', (req, res) =>
+    res.sendFile('index.html', {root: 'dist/read-local-json-angular/'}),
+);
+
+app.listen(process.env.PORT || 8080);
