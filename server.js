53525a66b0811e0415e7972e0f8b90948ac85d18
sweb1433 <sweb1433@gmail.com>
2020-06-12 14:02:46 +0530

    added compression in server js to gzip bundels,webpack-bundle-analyzer implemented

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+const compression = require("compression")
 const express = require('express');
-
 const app = express();
+app.use(compression())
 
 app.use(express.static('./dist/read-local-json-angular'));
 
